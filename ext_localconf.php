4c083ef81f7e32c7fc6fb18b1779b0075e4435c6
Stephan Kellermayr <stephan.kellermayr@gmail.com>
2024-07-09 10:56:13 +0200

    Wrap content of ext_localconf.php with function()

--- ext_localconf.php
+++ ext_localconf.php
@@ -2,7 +2,9 @@
 
 defined('TYPO3') || die();
 
-// Xclass ViewHelper
-$GLOBALS['TYPO3_CONF_VARS']['SYS']['Objects'][BK2K\BootstrapPackage\ViewHelpers\IconViewHelper::class] = [
+(function () {
+    // Override ViewHelper
+    $GLOBALS['TYPO3_CONF_VARS']['SYS']['Objects'][BK2K\BootstrapPackage\ViewHelpers\IconViewHelper::class] = [
     'className' => Quellenform\BootstrapPackageIconpack\ViewHelpers\IconViewHelper::class,
-];
+    ];
+})();
