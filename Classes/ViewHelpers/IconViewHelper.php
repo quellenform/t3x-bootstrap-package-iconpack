bb7815775c675f0eff5b657bf0e332b8465170e9
Stephan Kellermayr <stephan.kellermayr@gmail.com>
2024-10-21 17:21:18 +0200

    Drop deprecated renderStatic

--- Classes/ViewHelpers/IconViewHelper.php
+++ Classes/ViewHelpers/IconViewHelper.php
@@ -4,9 +4,7 @@ declare(strict_types=1);
 
 namespace Quellenform\BootstrapPackageIconpack\ViewHelpers;
 
-use TYPO3Fluid\Fluid\Core\Rendering\RenderingContextInterface;
 use TYPO3Fluid\Fluid\Core\ViewHelper\AbstractViewHelper;
-use TYPO3Fluid\Fluid\Core\ViewHelper\Traits\CompileWithRenderStatic;
 use TYPO3\CMS\Core\Utility\GeneralUtility;
 use Quellenform\Iconpack\IconpackFactory;
 
@@ -15,20 +13,12 @@ use Quellenform\Iconpack\IconpackFactory;
  */
 class IconViewHelper extends AbstractViewHelper
 {
-    use CompileWithRenderStatic;
-
     /**
      * @var bool
      */
     protected $escapeOutput = false;
 
-    /**
-     * Initialize arguments.
-     *
-     * @throws \TYPO3Fluid\Fluid\Core\ViewHelper\Exception
-     * @return void
-     */
-    public function initializeArguments()
+    public function initializeArguments(): void
     {
         parent::initializeArguments();
         $this->registerArgument('icon', 'mixed', 'Icon to render', true);
@@ -37,26 +27,19 @@ class IconViewHelper extends AbstractViewHelper
     }
 
     /**
-     * @param array $arguments
-     * @param \Closure $renderChildrenClosure
-     * @param RenderingContextInterface $renderingContext
      * @return string
-     * @throws \Exception
      */
-    public static function renderStatic(
-        array $arguments,
-        \Closure $renderChildrenClosure,
-        RenderingContextInterface $renderingContext
-    ) {
-        $icon = $arguments['icon'];
+    public function render(): string
+    {
+        $icon = $this->arguments['icon'];
 
         if (isset($icon) && \is_string($icon)) {
             $additionalAttributes = [];
-            if (isset($arguments['width'])) {
-                $additionalAttributes['width'] = $arguments['width'];
+            if (isset($this->arguments['width'])) {
+                $additionalAttributes['width'] = $this->arguments['width'];
             }
-            if (isset($arguments['height'])) {
-                $additionalAttributes['height'] = $arguments['height'];
+            if (isset($this->arguments['height'])) {
+                $additionalAttributes['height'] = $this->arguments['height'];
             }
             /** @var IconpackFactory $iconpackFactory */
             $iconpackFactory = GeneralUtility::makeInstance(IconpackFactory::class);
@@ -66,11 +49,11 @@ class IconViewHelper extends AbstractViewHelper
                 $additionalAttributes
             ) ?? '';
         } else {
-            if (isset($arguments['width'])) {
-                $icon->setWidth((int) $arguments['width']);
+            if (isset($this->arguments['width'])) {
+                $icon->setWidth((int) $this->arguments['width']);
             }
-            if (isset($arguments['height'])) {
-                $icon->setHeight((int) $arguments['height']);
+            if (isset($this->arguments['height'])) {
+                $icon->setHeight((int) $this->arguments['height']);
             }
 
             return $icon->render();
