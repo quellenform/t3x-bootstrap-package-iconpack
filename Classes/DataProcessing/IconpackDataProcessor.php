adb9fdfca1d6380344078a8ecd8467c4247f7be7
Stephan Kellermayr <stephan.kellermayr@gmail.com>
2025-09-24 18:01:36 +0200

    Updated the DataProcessor so that it works with Iconpack

--- Classes/DataProcessing/IconpackDataProcessor.php
+++ Classes/DataProcessing/IconpackDataProcessor.php
@@ -4,11 +4,19 @@ declare(strict_types=1);
 
 namespace Quellenform\BootstrapPackageIconpack\DataProcessing;
 
-use BK2K\BootstrapPackage\DataProcessing\IconsDataProcessor;
+/*
+ * This file is part of the "bootstrap_package_iconpack" Extension for TYPO3 CMS.
+ *
+ * For the full copyright and license information, please read the
+ * LICENSE.txt file that was distributed with this source code.
+ */
+
 use BK2K\BootstrapPackage\Icons\FileIcon;
 use BK2K\BootstrapPackage\Service\IconService;
+use Quellenform\BootstrapPackageIconpack\Icons\IconpackIcon;
 use TYPO3\CMS\Core\Utility\GeneralUtility;
 use TYPO3\CMS\Frontend\ContentObject\ContentObjectRenderer;
+use TYPO3\CMS\Frontend\ContentObject\DataProcessorInterface;
 use TYPO3\CMS\Frontend\DataProcessing\FilesProcessor;
 
 /**
@@ -16,13 +24,13 @@ use TYPO3\CMS\Frontend\DataProcessing\FilesProcessor;
  *
  * 10 = Quellenform\BootstrapPackageIconpack\DataProcessing\IconpackDataProcessor
  * 10 {
- *   iconSetFieldName = iconpack_enable
- *   iconIdentifierFieldName = iconpack
+ *   iconSetFieldName = icon_set
+ *   iconIdentifier.field = icon
  *   iconFileFieldName = icon_file
  *   as = icon
  * }
  */
-class IconpackDataProcessor extends IconsDataProcessor
+class IconpackDataProcessor implements DataProcessorInterface
 {
     /**
      * @var IconService
@@ -42,19 +50,27 @@ class IconpackDataProcessor extends IconsDataProcessor
      * @param array $contentObjectConfiguration The configuration of Content Object
      * @param array $processorConfiguration The configuration of this processor
      * @param array $processedData Key/value store of processed data (e.g. to be passed to a Fluid View)
+     *
      * @return array the processed data as key/value store
      */
-    public function process(ContentObjectRenderer $cObj, array $contentObjectConfiguration, array $processorConfiguration, array $processedData)
-    {
+    public function process(
+        ContentObjectRenderer $cObj,
+        array $contentObjectConfiguration,
+        array $processorConfiguration,
+        array $processedData
+    ): array {
         $icon = null;
         $config = [
             'iconSet' => (string) $cObj->stdWrapValue('iconSet', $processorConfiguration, ''),
             'iconIdentifier' => (string) $cObj->stdWrapValue('iconIdentifier', $processorConfiguration, ''),
-            'iconFileFieldName' => (string) $cObj->stdWrapValue('iconFileFieldName', $processorConfiguration, ''),
+            'iconFileFieldName' => (string) $cObj->stdWrapValue('iconFileFieldName', $processorConfiguration, '')
         ];
-
-        if (boolval($config['iconSet']) && $config['iconIdentifier'] !== '') {
-            $icon = $config['iconIdentifier'];
+        if ((bool) $config['iconSet'] && $config['iconIdentifier'] !== '') {
+            // Return a simple IconpackIcon without a preview image as we will only need the identifier later.
+            $icon = (new IconpackIcon())
+                ->setIdentifier($config['iconIdentifier'])
+                ->setName($config['iconIdentifier'])
+                ->setPreviewImage('') ?? null;
         } elseif ($config['iconFileFieldName'] !== '') {
             $filesProcessor = GeneralUtility::makeInstance(FilesProcessor::class);
             $filesData = $filesProcessor->process(
